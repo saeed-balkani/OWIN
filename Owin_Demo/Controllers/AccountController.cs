e32fb815954778ddcceb285e63e93badbd4695e4
saeed-balkani <saeedbalkani@gmail.com>
2020-12-22 15:01:38 +0100

    init1

---
+++ Owin_Demo/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Owin_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Owin_Demo.Controllers
+{
+    public class AccountController : Controller
+    {
+        List<User> db = new List<User>();
+        
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            LoginViewModel login = new LoginViewModel();
+            login.RecentUrl = returnUrl;
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel model)
+        {
+            db.Add(new User
+            {
+                UserId = 1,
+                Username = "@saeed.balkani",
+                Email = "test@yahoo.com",
+                Password = "908070"
+            });
+
+            if (ModelState.IsValid)
+            {
+                if (db.Any(a=>a.Email == model.Email && a.Password == model.Password))
+                {
+                    var identity = new ClaimsIdentity(new[] 
+                    {
+                        new Claim(ClaimTypes.Email, model.Email)
+                    }, "ApplicationCookie");
+
+                    var context = Request.GetOwinContext();
+                    var authManager = context.Authentication;
+                    authManager.SignIn(identity);
+                    return Redirect(model.RecentUrl);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Owin_Demo/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Owin_Demo.Models
+{
+
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+
+}
